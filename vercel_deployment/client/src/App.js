243a0e1f01a83e789d95d65e9a37c13f055cbb3f
cesarroncon <100439739+cesarroncon@users.noreply.github.com>
2024-03-27 12:17:55 +0000

    Update App.js

--- vercel_deployment/client/src/App.js
+++ vercel_deployment/client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+function DataReceiver() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Função para buscar os dados do servidor
+    const fetchData = async () => {
+      try {
+        //const response = await axios.get('http://localhost:5000/'); // Faz a solicitação GET para o servidor
+        const response = await axios.get('https://vercel-repositorio-server.vercel.app/');
+        setData(response.data); // Define os dados recebidos no estado
+      } catch (error) {
+        console.error('Erro ao buscar os dados do servidor:', error);
+      }
+    };
+
+    fetchData(); // Chama a função para buscar os dados quando o componente é montado
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Dados Recebidos do Servidor:</h2>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre> // Exibe os dados recebidos
+      ) : (
+        <p>Carregando...</p> // Exibe "Carregando..." enquanto aguarda os dados
+      )}
     </div>
   );
 }
 
-export default App;
+export default DataReceiver;
